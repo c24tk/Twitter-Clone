0c9f08c45bb93b6f57322837e85c6c69dc516bb7
Tara Kamshad <c24tk@dalton.org>
2022-10-19 15:00:42 -0400

    more updates

--- scripts.js
+++ scripts.js
@@ -6,15 +6,30 @@ const tweet = document.getElementById('tweet');
 const ul = document.getElementById('timeline');
 const li = document.createElement('li');
 
+let currentDate = new Date();
+let cDay = currentDate.getDate()
+let cMonth = currentDate.getMonth() + 1
+let cYear = currentDate.getFullYear()
 
+let time =    " " + cMonth + "/" + cDay + "/"+ cYear
+let time1 = ", " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
 
 if (tweet.value.length===0){
      return false;
     }
+let span = document.createElement('span');
+span.className = "time";
+span.appendChild(document.createTextNode(time + time1))
 
-li.appendChild(document.createTextNode(tweet.value,getDate() ));
+let span2 = document.createElement('span');
+span2.className = "user";
+span2.appendChild(document.createTextNode("@tarakamshad"))
+
+li.appendChild(document.createTextNode(tweet.value))
+li.appendChild(span);
+li.appendChild(span2);
+
 ul.appendChild(li);
-
 
 tweet.value = null
 return false;
